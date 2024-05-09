d40134b0e8ee8d59e1bf80a5d45c67fe3785bfb4
emanzaid12 <emanzaid139@gmail.com>
2024-05-09 23:50:59 +0300

    added blod image and blog js

---
+++ blog.js
@@ -0,0 +1,39 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onclick=() =>{
+    navbar.classList.toggle('active');
+}
+
+
+
+let search = document.querySelector('.search');
+document.querySelector('#search').onclick=() =>{
+    search.classList.toggle('active');
+}
+var swiper = new Swiper(".blogs-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation:{
+        nextE1 :".swiper-button-next",
+        prevE1 :".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 1,
+      },
+      1024: {
+        slidesPerView: 1,
+      },
+    },
+  });
