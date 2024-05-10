908c4485e12cad14fd4dff11ebd05eb65e40cfdf
emanzaid12 <emanzaid139@gmail.com>
2024-05-11 00:40:47 +0300

    solving problems

--- blog.js
+++ blog.js
@@ -11,29 +11,29 @@ document.querySelector('#search').onclick=() =>{
 }
 var swiper = new Swiper(".blogs-row", {
     spaceBetween: 30,
-    loop:true,
-    centeredSlides:true,
-    autoplay:{
-        delay:9500,
-        disableOnInteraction:false,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false // Removed the extra comma here
     },
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
-    navigation:{
-        nextE1 :".swiper-button-next",
-        prevE1 :".swiper-button-prev",
+    navigation: {
+        nextEl: ".swiper-button-next", // Corrected property names
+        prevEl: ".swiper-button-prev", // Corrected property names
     },
     breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 1,
-      },
-      1024: {
-        slidesPerView: 1,
-      },
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 1,
+        },
+        1024: {
+            slidesPerView: 1,
+        },
     },
-  });
+});
