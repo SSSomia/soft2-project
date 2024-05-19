49ffa1d061da97bd0055716845df1f62bb1e717f
SSSomia <smiasrour>
2024-05-19 09:16:39 +0300

    changes

--- review.js
+++ review.js
@@ -1,15 +1,3 @@
-let navbar = document.querySelector('.navbar');
-document.querySelector('#menu-bar').onclick=() =>{
-    navbar.classList.toggle('active');
-}
-
-
-
-let search = document.querySelector('.search');
-document.querySelector('#search').onclick=() =>{
-    search.classList.toggle('active');
-}
-
 
   var swiper = new Swiper(".review-row", {
     spaceBetween: 30,
